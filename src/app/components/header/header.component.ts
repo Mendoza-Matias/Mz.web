74a77bc84431e18cfd583ce4c837320e92ea2cb6
matias <mmendoza@gmail.com>
2025-03-24 17:54:25 -0300

    modificacion del header y el footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-header',
-  imports: [HeroComponent],
+  standalone: true,
+  imports: [CommonModule, RouterModule, HeroComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
